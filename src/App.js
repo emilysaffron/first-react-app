e1921c1a05af179576072e2dfa0fc8e2a282b377
emilysaffron <emiy.saffron@bbc.co.uk>
2020-10-27 16:24:41 +0000

    prettier

--- src/App.js
+++ src/App.js
@@ -1,35 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import Button from "./Button";
-import {useState} from "react";
+import { useState } from "react";
 function App() {
-  const [spin,updateSpin]=useState("logo");
-  const pos = ["logo","stopped-logo"];
+  const [spin, updateSpin] = useState("logo");
+  const pos = ["logo", "stopped-logo"];
   const stopSpin = () => {
-   
-   updateSpin(pos[0]===spin?pos[1]:pos[0]);
-   spin===pos[1]?document.getElementById(pos[1]).style.animationPlayState='running':document.getElementById(pos[0]).style.animationPlayState='paused';
-   
-  
-   
-  }  
+    updateSpin(pos[0] === spin ? pos[1] : pos[0]);
+    spin === pos[1]
+      ? (document.getElementById(pos[1]).style.animationPlayState = "running")
+      : (document.getElementById(pos[0]).style.animationPlayState = "paused");
+  };
 
- 
- 
-    
-
-
- console.log(spin);
+  console.log(spin);
   return (
     <div className="App">
       <header className="App-header">
-      <img src={logo} id={spin} className="App-logo" alt="logo" /> 
-      
-      <div onClick={stopSpin}>
-        
-        <Button/>
+        <img src={logo} id={spin} className="App-logo" alt="logo" />
+
+        <div onClick={stopSpin}>
+          <Button />
         </div>
-        
       </header>
     </div>
   );
